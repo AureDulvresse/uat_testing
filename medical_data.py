46cd07b5e7f230ef879726ed3c8aa5b69e214b64
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-06-03 13:43:39 +0100

    Update medical_data.py

--- medical_data.py
+++ medical_data.py
@@ -1,14 +1,29 @@
 import logging
+from colorlog import ColoredFormatter
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 import time
 
 class Actions:
     # Initialisation du logger
-    logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger("Actions")
+    formatter = ColoredFormatter(
+        "%(log_color)s%(levelname)s: %(message)s",
+        log_colors={
+            'DEBUG':    'cyan',
+            'INFO':     'green',
+            'WARNING':  'yellow',
+            'ERROR':    'red',
+            'CRITICAL': 'red,bg_white',
+        },
+        reset=True,
+        style='%'
+    )
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    logger.setLevel(logging.INFO)
 
     @staticmethod
     def access_configuration(driver):
@@ -22,13 +37,14 @@ class Actions:
             None
         """
         try:
-            # Accès au module "Configuration"
-            configuration_module = driver.find_element(By.XPATH, "//*[contains(text(), 'Configuration')]")
+            # Attendre que l'élément soit cliquable et cliquer
+            configuration_module = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Configuration')]"))
+            )
             configuration_module.click()
-            time.sleep(2)
-            Actions.logger.info("\033[92mAccès réussi au module Configuration.\033[0m")
+            Actions.logger.info("Accès réussi au module Configuration.")
         except Exception as e:
-            Actions.logger.error("\033[91mÉchec de l'accès au module Configuration : %s\033[0m", str(e))
+            Actions.logger.error("Échec de l'accès au module Configuration : %s", str(e))
 
     @staticmethod
     def access_general_data(driver):
@@ -42,14 +58,14 @@ class Actions:
             None
         """
         try:
+            # Accéder au module "Configuration" d'abord
+            Actions.access_configuration(driver)
             
-            # Accès aux "Données générales"
-            # Cliquer sur l'élément contenant le texte "Données médicales"
-            donnee_medicale_element = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "//*[contains(@class, 'div_class_name') and contains(., 'Données générales')]"))
+            # Attendre que l'élément soit visible et cliquable, puis cliquer en utilisant JavaScript
+            general_data_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[contains(@class, 'div_class_name') and contains(., 'Données générales')]"))
             )
-            donnee_medicale_element.click()
-            time.sleep(2)
-            Actions.logger.info("\033[92mAccès réussi aux Données générales.\033[0m")
+            driver.execute_script("arguments[0].click();", general_data_element)
+            Actions.logger.info("Accès réussi aux Données générales.")
         except Exception as e:
-            Actions.logger.error("\033[91mÉchec de l'accès aux Données générales : %s\033[0m", str(e))
+            Actions.logger.error("Échec de l'accès aux Données générales : %s", str(e))
